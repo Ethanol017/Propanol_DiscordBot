41e53e808f859431825e77cd305b70a8b4a4f7fe
Ethanol017 <ethanol017217@gmail.com>
2024-08-25 10:36:14 +0000

    DynamicVoiceChannel notification

--- cogs/DynamicVoiceChannel.py
+++ cogs/DynamicVoiceChannel.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import configparser
+import datetime
+import json
 
 class DynamicVoiceChannel(commands.Cog):
     def __init__(self, bot):
@@ -27,6 +29,7 @@ class DynamicVoiceChannel(commands.Cog):
                     overwrites=overwrites,
                     category=category  
                 )
+                await self.create_notification(guild,member,new_channel.mention)
 
                 await member.move_to(new_channel)
 
@@ -36,5 +39,40 @@ class DynamicVoiceChannel(commands.Cog):
                 await self.bot.wait_for('voice_state_update', check=check)
                 await new_channel.delete()
 
+    async def create_notification(self, guild : discord.Guild ,member,channel_mention):
+        embed = discord.Embed(title="語音頻道創建", description="", color=0x30D5C8)
+        embed.add_field(name="頻道", value=channel_mention, inline=True)
+        embed.add_field(name="創建者", value=member.display_name, inline=True)
+        with open('DynamicVoiceNotificationList.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            notification_list = data[str(member.id)]
+        notification_str = " ".join(f"<@{x}>" for x in notification_list)
+        print(notification_str)
+        if notification_str == "":
+            notification_str = "無"
+        embed.add_field(name="通知", value=notification_str, inline=False)
+        embed.timestamp = datetime.datetime.now(datetime.UTC)
+        channelID = self.config['DynamicVoiceChannel'].getint('notification_Channel')
+        await guild.get_channel(channelID).send(embed=embed)
+    
+    @app_commands.command(name='訂閱語音通知')
+    async def subscription(self,interaction: discord.Interaction, member: discord.Member):
+        with open('DynamicVoiceNotificationList.json', 'r+', encoding='utf-8') as f:
+                data = json.load(f)
+                data.setdefault(str(member.id),[])
+                data[str(member.id)].append(interaction.user.id)
+                f.seek(0)
+                json.dump(data, f, indent=4, separators=(',', ': '))
+                f.truncate()
+        await interaction.response.send_message('設置成功',ephemeral=True)
+    @app_commands.command(name='取消訂閱語音通知')
+    async def unsubscription(self,interaction: discord.Interaction, member: discord.Member):
+        with open('DynamicVoiceNotificationList.json', 'r+', encoding='utf-8') as f:
+                data = json.load(f)
+                data[str(member.id)].remove(interaction.user.id)
+                f.seek(0)
+                json.dump(data, f, indent=4, separators=(',', ': '))
+                f.truncate()
+        await interaction.response.send_message('設置成功',ephemeral=True)
 async def setup(bot):
     await bot.add_cog(DynamicVoiceChannel(bot))
