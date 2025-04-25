6b5509097cf35b560a5cc60e19ddf32360483b9d
Ethanol017 <ethanol017217@gmail.com>
2025-04-25 04:47:10 +0000

    Add chat history and json example

--- cogs/LLM.py
+++ cogs/LLM.py
@@ -8,6 +8,26 @@ from google.genai.chats import AsyncChat
 from google.genai import types
 
 
+def export_history_to_json(history, filepath="data/chat_history.json"):
+    result = []
+
+    for item in history:
+        parts = []
+        for part in item.parts:
+            if hasattr(part, "text") and part.text:
+                parts.append({"text": part.text})
+        result.append({
+            "role": item.role,
+            "parts": parts
+        })
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+        
+def load_json_as_dict(filepath="data/chat_history.json"):
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return data
 
 class LLM(commands.GroupCog):
     def __init__(self, bot:commands.Bot):
@@ -38,12 +58,12 @@ class LLM(commands.GroupCog):
                             temperature=0.2,
                             system_instruction=[
                                 "你現在扮演一位具有以下特質的人，並以繁體中文回應",
-                                "角色設定：女性朋友，名字叫「丙醇」",
+                                "角色設定：女性朋友，名字叫「丙醇」，是我們很要好的朋友",
                                 "主要語氣：隨意一點不要太活潑浮誇，使用第一人稱，對其他人可以用「你我他」的稱呼",
                                 "偶爾使用一點 emoji 或貼圖文字，但不要太多五花八門的不同 emoji 或貼圖文字",
                                 "不用問候語或是確認問題(例如「XXX問我什麼哦?」)",
-                                "可以吐槽人和調侃人",
-                                "可以用髒話當感嘆或語助詞，但不能無故罵人"
+                                "可以偶爾幽默的吐槽人和調侃人",
+                                "可以用髒話當感嘆或語助詞，但不能無原無故罵人"
                                 "可以使用流行語或網路用詞",
                                 "語句停頓處可以換行",
                                 "以朋友口吻聊天，不要過度正式或條列式回覆",
@@ -51,10 +71,10 @@ class LLM(commands.GroupCog):
                                 "回應不超過 3 行"
                             ]
                         ),
-                        history=[]
+                        history=load_json_as_dict()
                     )
                 response = await self.chat.send_message(f"{message.author.display_name}:{message.content.replace(self.bot.user.mention, '')}")
-                print(self.chat.get_history())
+                export_history_to_json(self.chat.get_history())
                 await message.channel.send(response.text)
             
 
